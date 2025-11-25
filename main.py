bc71e71c15c167396819e3c646a0108004b5b5c5
William Victoria <willivictoria1413@gmail.com>
2025-11-25 09:29:00 -0500

    Crear API Movie App

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+import mysql.connector
+from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME")
+    )
+
+@app.get("/")
+def home():
+    return {"message": "API funcionando proyecto final computación en la nube grupo 05"}
+
+@app.get("/series")
+def get_series():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM series")
+    result = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return result
+
+@app.get("/series/{id}")
+def get_serie_by_id(id: int):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM series WHERE serie_id = %s", (id,))
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if not result:
+        raise HTTPException(status_code=404, detail="Serie not found")
+
+    return result
